ac68a33008307fcb4b1cd1aaf71a848d73c2308c
Imon <contact@imonweb.co.uk>
2023-06-10 22:35:17 +0100

    shop

---
+++ src/context/Shop-context.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { PRODUCTS } from '../products';
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+  let cart = {}
+  for(let i = 1; i < PRODUCTS.length + 1; i++){
+    cart[i] = 0
+  }
+  return cart;
+}
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setcartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setcartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1 })) 
+  }
+
+  const removeFromCart = (itemId) => {
+    setcartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 })) 
+  }
+
+  const contextValue = {cartItems, addToCart, removeFromCart }
+
+  // console.log(cartItems)
+
+  return (
+ <>
+ <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
+ </>
+ 
+  )
+}
+
+export default ShopContextProvider
---
+++ src/pages/shop/Product.jsx
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react'
+import { ShopContext } from '../../context/Shop-context'
+const Product = (props) => {
+  const {id, productName, price, productImage} = props.data
+  const {addToCart} = useContext(ShopContext)
+
+  return (
+    <div className='product'> 
+      <img src={productImage} alt="" />
+      <div className="description">
+        <p><b>{productName}</b></p>
+        <p>£{price}</p>
+      </div>
+      <button className='addToCartBttn' onClick={() => addToCart(id)}>Add To Cart</button>
+    </div>
+  )
+}
+
+export default Product
